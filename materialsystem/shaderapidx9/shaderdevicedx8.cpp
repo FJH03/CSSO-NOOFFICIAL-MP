1f5820074e99ea8a7d5773bafbc1f7c71f53b482
Evgeniy Sidorenko <zarod2005@yandex.ru>
2022-06-11 02:46:15 +0300

    set correct number of constants for PS 1.0

--- materialsystem/shaderapidx9/shaderdevicedx8.cpp
+++ materialsystem/shaderapidx9/shaderdevicedx8.cpp
@@ -799,9 +799,13 @@ bool CShaderDeviceMgrDx8::ComputeCapsFromD3D( HardwareCaps_t *pCaps, int nAdapte
 		{
 			pCaps->m_NumPixelShaderConstants = 224;
 		}
-		else
+		else if ( pCaps->m_SupportsPixelShaders_2_0 )
 		{
 			pCaps->m_NumPixelShaderConstants = 32;
+		}
+		else
+		{
+			pCaps->m_NumPixelShaderConstants = 8;
 		}
 	}
 	else
