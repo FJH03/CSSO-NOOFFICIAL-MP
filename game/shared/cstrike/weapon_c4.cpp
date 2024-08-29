813c648ca54548151a532861f7cf0677d64ee84a
modniykrokyy <yakovlev1986danyal@gmail.com>
2024-08-29 13:57:43 +0300

    frizetime

--- game/shared/cstrike/weapon_c4.cpp
+++ game/shared/cstrike/weapon_c4.cpp
@@ -322,7 +322,11 @@ END_PREDICTION_DATA()
 		}
 				
 
-		SetNextThink( gpGlobals->curtime + 0.12 );
+		bool bCloseToExplode = (m_flC4Blow - 3.0 <= gpGlobals->curtime);
+		if ( bCloseToExplode || m_bStartDefuse )
+			SetNextThink( gpGlobals->curtime );
+		else
+			SetNextThink( gpGlobals->curtime + 0.12 );
 
 #ifndef CLIENT_DLL
 		// let the bots hear the bomb beeping
