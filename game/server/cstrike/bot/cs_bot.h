d0efb994b799fd9d11a105918d54e60a6c264e37
modniykrokyy <yakovlev1986danyal@gmail.com>
2024-08-30 22:36:23 +0300

    allowing for +USEing the bots (to take c4)

--- game/server/cstrike/bot/cs_bot.h
+++ game/server/cstrike/bot/cs_bot.h
@@ -1276,6 +1276,8 @@ private:
 	float m_voiceEndTimestamp;
 
 	BotChatterInterface m_chatter;									///< chatter mechanism
+
+	int ObjectCaps( void ) { return ( BaseClass::ObjectCaps() | FCAP_IMPULSE_USE ); } //allow +use
 };
 
 
