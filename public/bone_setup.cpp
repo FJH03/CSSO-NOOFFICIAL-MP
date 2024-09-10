096375ef578f1b5a22ae0e7c5d8b6d8b3578bf7e
DMK95 <maga200544@gmail.com>
2024-09-10 15:55:11 +0300

    some IK code changes

--- public/bone_setup.cpp
+++ public/bone_setup.cpp
@@ -2638,14 +2638,14 @@ public:
          X[i] = P[i];
       normalize(X);
 
-// Its y axis is perpendicular to P, so Y = unit( E - X(E·X) ).
+// Its y axis is perpendicular to P, so Y = unit( E - X(Eï¿½X) ).
 
       float dDOTx = dot(D,X);
       for (i = 0 ; i < 3 ; i++)
          Y[i] = D[i] - dDOTx * X[i];
       normalize(Y);
 
-// Its z axis is perpendicular to both X and Y, so Z = X×Y.
+// Its z axis is perpendicular to both X and Y, so Z = Xï¿½Y.
 
       cross(X,Y,Z);
 
@@ -2703,6 +2703,7 @@ void debugLine(const Vector& origin, const Vector& dest, int r, int g, int b, bo
 //-----------------------------------------------------------------------------
 bool Studio_SolveIK( mstudioikchain_t *pikchain, Vector &targetFoot, matrix3x4_t *pBoneToWorld )
 {
+#if 0
 	if (pikchain->pLink(0)->kneeDir.LengthSqr() > 0.0)
 	{
 		Vector targetKneeDir, targetKneePos;
@@ -2713,6 +2714,7 @@ bool Studio_SolveIK( mstudioikchain_t *pikchain, Vector &targetFoot, matrix3x4_t
 		return Studio_SolveIK( pikchain->pLink( 0 )->bone, pikchain->pLink( 1 )->bone, pikchain->pLink( 2 )->bone, targetFoot, targetKneePos, targetKneeDir, pBoneToWorld );
 	}
 	else
+#endif
 	{
 		return Studio_SolveIK( pikchain->pLink( 0 )->bone, pikchain->pLink( 1 )->bone, pikchain->pLink( 2 )->bone, targetFoot, pBoneToWorld );
 	}
@@ -2747,6 +2749,13 @@ bool Studio_SolveIK( int iThigh, int iKnee, int iFoot, Vector &targetFoot, matri
 
 	// leg too straight to figure out knee?
 	if (l3 > (l1 + l2) * KNEEMAX_EPSILON)
+	{
+		return false;
+	}
+
+	// If any of the thigh to knee to foot bones are co-positional, then solving ik doesn't make sense. 
+	// We're probably looking at uninitialized bones or something
+	if ( l1 <= 0 || l2 <= 0 || l3 <= 0 )
 	{
 		return false;
 	}
@@ -4076,7 +4085,7 @@ void CIKContext::SolveDependencies( Vector pos[], Quaternion q[], matrix3x4_t bo
 		int bone = pchain->pLink( 2 )->bone;
 
 		pChainResult->target = -1;
-		pChainResult->flWeight = 0.0;
+		pChainResult->flWeight = 0.0f;
 
 		// don't bother with chain if the bone isn't going to be calculated
 		if ( !(m_pStudioHdr->boneFlags( bone ) & m_boneMask))
@@ -4116,7 +4125,7 @@ void CIKContext::SolveDependencies( Vector pos[], Quaternion q[], matrix3x4_t bo
 					}
 			
 					float flWeight = pRule->flWeight * pRule->flRuleWeight;
-					pChainResult->flWeight = pChainResult->flWeight * (1 - flWeight) + flWeight;
+					pChainResult->flWeight = pChainResult->flWeight * (1.0f - flWeight) + flWeight;
 
 					Vector p2;
 					Quaternion q2;
@@ -4127,7 +4136,7 @@ void CIKContext::SolveDependencies( Vector pos[], Quaternion q[], matrix3x4_t bo
 					// debugLine( pChainResult->pos, p2, 0, 0, 255, true, 0.1 );
 
 					// blend in position and angles
-					pChainResult->pos = pChainResult->pos * (1.0 - flWeight) + p2 * flWeight;
+					pChainResult->pos = pChainResult->pos * (1.0f - flWeight) + p2 * flWeight;
 					QuaternionSlerp( pChainResult->q, q2, flWeight, pChainResult->q );
 				}
 				break;
@@ -4184,9 +4193,9 @@ void CIKContext::SolveDependencies( Vector pos[], Quaternion q[], matrix3x4_t bo
 			//mstudioikchain_t *pchain = m_pStudioHdr->pIKChain( m_target[i].chain );
 			ikchainresult_t *pChainResult = &chainResult[ pTarget->chain ];
 
-			AngleMatrix(pTarget->offset.q, pTarget->offset.pos, local );
+			AngleMatrix( RadianEuler(pTarget->offset.q), pTarget->offset.pos, local );
 
-			AngleMatrix( pTarget->est.q, pTarget->est.pos, worldFootpad );
+			AngleMatrix( RadianEuler(pTarget->est.q), pTarget->est.pos, worldFootpad );
 
 			ConcatTransforms( worldFootpad, local, worldTarget );
 
