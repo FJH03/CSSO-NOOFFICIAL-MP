981ee1f0ec9f23f182387daf7303da053d03286e
DMK95 <maga200544@gmail.com>
2024-09-12 15:38:39 +0300

    fixed some team checks

--- game/shared/cstrike/cs_gamerules.cpp
+++ game/shared/cstrike/cs_gamerules.cpp
@@ -1742,7 +1742,7 @@ ConVar snd_music_selection(
 				if(	pCSExplosionAttacker &&                  
 					!pCSExplosionVictim->IsAlive() && 
 					wasAliveBeforeExplosion &&
-					pCSExplosionVictim->GetTeamNumber() != pCSExplosionAttacker->GetTeamNumber())               
+					pCSExplosionVictim->IsOtherEnemy(pCSExplosionAttacker)) 
 				{
 					numberOfEnemyPlayersKilledByThisExplosion++;
 				}
