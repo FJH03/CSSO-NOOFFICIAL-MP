1118994e4925c0a561bc703322b72182e6771d97
DMK95 <maga200544@gmail.com>
2024-09-14 15:25:32 +0300

    removed a bit of obsolete shit from previous commit and changed mp_gamemode_override description

--- game/shared/cstrike/cs_gamerules.cpp
+++ game/shared/cstrike/cs_gamerules.cpp
@@ -503,7 +503,7 @@ ConVar mp_gamemode_override(
 	"mp_gamemode_override",
 	"0",
 	FCVAR_REPLICATED,
-	"What gamemode are we playing today?\n 0 - Custom\n 1 - Casual\n 2 - Competitive\n 3 - Wingman\n 4 - Deathmatch",
+	"What gamemode are we playing today?",
 	true, 0,
 	true, GameModes::NUM_GAMEMODES - 1 );
 
@@ -7143,7 +7143,7 @@ void CCSGameRules::AddTeamAccount( int team, int reason )
 {
 	if ( !mp_teamcashawards.GetBool() )
 		return;
-	
+
 	int amount = TeamCashAwardValue( reason );
 
 	AddTeamAccount( team, reason, amount );
