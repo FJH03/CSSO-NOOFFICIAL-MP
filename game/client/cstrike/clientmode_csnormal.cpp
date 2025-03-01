3b36c9d2dcb736478c2e16ecf49faafb565db726
DMK95 <maga200544@gmail.com>
2025-03-01 20:10:49 +0300

    use a nowep sequence in buymenu if no weapons

--- game/client/cstrike/clientmode_csnormal.cpp
+++ game/client/cstrike/clientmode_csnormal.cpp
@@ -1310,6 +1310,7 @@ void UpdateBuyMenuImageEntity(
 		{
 			Warning( "UpdateBuyMenuImageEntity: Unable to find active weapon for player." );
 			return;
+			pWeaponSequence = "t_buymenu_nowep";
 		}
 	}
 	else
@@ -1400,6 +1401,12 @@ void UpdateBuyMenuImageEntity(
 		if ( silencerBodygroup > -1 )
 			pWeaponModel->SetBodygroup( silencerBodygroup, m_bSilenced ? 0 : 1 );
 		g_BuyMenuImageWeapon = pWeaponModel;
+	}
+	else if ( !pWeaponName || !pWeaponName[0] )
+	{
+		// so the weapon model is gone when playing a nowep sequence
+		if ( pWeaponModel )
+			pWeaponModel->Remove();
 	}
 
 	C_BaseAnimating *pGlovesModel = g_BuyMenuImageGloves.Get();
