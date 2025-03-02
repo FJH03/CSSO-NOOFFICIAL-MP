3ce2135ef48c1c1a9b584a759b730dfeb83f69f7
DMK95 <maga200544@gmail.com>
2025-03-02 17:37:20 +0300

    fixed incorrect poseparam values on client

--- game/client/c_baseanimating.h
+++ game/client/c_baseanimating.h
@@ -216,8 +216,8 @@ public:
 	float	SetPoseParameter( CStudioHdr *pStudioHdr, int iParameter, float flValue );
 	inline float SetPoseParameter( int iParameter, float flValue ) { return SetPoseParameter( GetModelPtr(), iParameter, flValue ); }
 
-	float	GetPoseParameter( int iPoseParameter );
-
+	float	GetPoseParameter( int iParameter );
+	
 	bool	GetPoseParameterRange( int iPoseParameter, float &minValue, float &maxValue );
 
 	int		LookupBone( const char *szName );
