25ee95a3c31a76b5fe56e65705c8c9b445c10786
DMK95 <maga200544@gmail.com>
2025-03-02 13:17:52 +0300

    don't swap active weapon for loadout weapon in buy menu and class menu

--- game/client/cstrike/clientmode_csnormal.cpp
+++ game/client/cstrike/clientmode_csnormal.cpp
@@ -1087,6 +1087,7 @@ void UpdateImageEntity(
 	if ( !szPlayerModel || !szPlayerModel[0] )
 		szPlayerModel = modelinfo->GetModelName( pLocalPlayer->GetModel() );
 
+	bool bActiveWeapon = false;
 	if ( !szWeaponClassname || !szWeaponClassname[0] )
 	{
 		C_BaseCombatWeapon *pPrimaryWeapon = pLocalPlayer->Weapon_GetSlot( WEAPON_SLOT_RIFLE );
@@ -1095,13 +1096,25 @@ void UpdateImageEntity(
 		C_BaseCombatWeapon *pActiveWeapon = pLocalPlayer->GetActiveWeapon();
 
 		if ( pPrimaryWeapon )
+		{
 			szWeaponClassname = pPrimaryWeapon->GetClassname();
+			bActiveWeapon = true;
+		}
 		else if ( pSecondaryWeapon )
+		{
 			szWeaponClassname = pSecondaryWeapon->GetClassname();
+			bActiveWeapon = true;
+		}
 		else if ( pKnifeWeapon )
+		{
 			szWeaponClassname = pKnifeWeapon->GetClassname();
+			bActiveWeapon = true;
+		}
 		else if ( pActiveWeapon )
+		{
 			szWeaponClassname = pActiveWeapon->GetClassname();
+			bActiveWeapon = true;
+		}
 		else if ( bIsClassSelection )
 		{
 			szWeaponClassname = "weapon_ak47";
@@ -1129,11 +1142,15 @@ void UpdateImageEntity(
 	}
 	else
 	{
-		const char* szLoadoutWeapon = CSLoadout()->GetWeaponFromSlot( pLocalPlayer, CSLoadout()->GetSlotFromWeapon( iTeamNumber, szWeaponClassname + 7 ) ); // +7 to get rid of weapon_ prefix
-		if ( szLoadoutWeapon && szLoadoutWeapon[0] )
-			szWeaponClassname = UTIL_VarArgs( "weapon_%s", szLoadoutWeapon );
+		// don't swap active weapon for a loadout one
+		if ( !bActiveWeapon )
+		{
+			const char* szLoadoutWeapon = CSLoadout()->GetWeaponFromSlot( pLocalPlayer, CSLoadout()->GetSlotFromWeapon( iTeamNumber, szWeaponClassname + 7 ) ); // +7 to get rid of weapon_ prefix
+			if ( szLoadoutWeapon && szLoadoutWeapon[0] )
+				szWeaponClassname = UTIL_VarArgs( "weapon_%s", szLoadoutWeapon );
+		}
 
-			WEAPON_FILE_INFO_HANDLE	hWpnInfo = LookupWeaponInfoSlot( szWeaponClassname );
+		WEAPON_FILE_INFO_HANDLE	hWpnInfo = LookupWeaponInfoSlot( szWeaponClassname );
 		if ( hWpnInfo == GetInvalidWeaponInfoHandle() )
 		{
 			if ( Q_strcmp( szWeaponClassname, "item_defuser" ) == 0 )
