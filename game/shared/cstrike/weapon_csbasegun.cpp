9c71e35d2b4449bf7fbc25dfbbed457051de5c20
DMK95 <maga200544@gmail.com>
2025-03-09 18:43:58 +0300

    fixed incorrect condition in GetDeployActivity

--- game/shared/cstrike/weapon_csbasegun.cpp
+++ game/shared/cstrike/weapon_csbasegun.cpp
@@ -92,7 +92,7 @@ bool CWeaponCSBaseGun::Deploy()
 
 Activity CWeaponCSBaseGun::GetDeployActivity( void )
 {
-	if ( IsSilenced() == Silencer_Removable )
+	if ( HasSilencer() == Silencer_Removable )
 	{
 		return ACT_VM_DRAW_SILENCED;
 	}
