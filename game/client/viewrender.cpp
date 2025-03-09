37902b39b32419fd9286fa6573d3f0094de147dc
DMK95 <maga200544@gmail.com>
2025-03-09 12:31:14 +0300

    fixed incorrect call to BuildWorldRenderLists()

--- game/client/viewrender.cpp
+++ game/client/viewrender.cpp
@@ -4798,7 +4798,7 @@ void CSkyboxView::DrawInternal( view_id_t iSkyBoxViewID, bool bInvokePreAndPostR
 	}
 
 	render->BeginUpdateLightmaps();
-	BuildWorldRenderLists( true, true, -1 );
+	BuildWorldRenderLists( true, -1, true );
 	BuildRenderableRenderLists( iSkyBoxViewID );
 	render->EndUpdateLightmaps();
 
