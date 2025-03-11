0c9db273d7b76c5ff95445636485fff1fcc6e121
DMK95 <maga200544@gmail.com>
2025-03-11 21:12:33 +0300

    doors now breakable

--- game/client/physpropclientside.cpp
+++ game/client/physpropclientside.cpp
@@ -225,7 +225,7 @@ int C_PhysPropClientside::ParsePropData( void )
 		return PARSE_FAILED_NO_DATA;
 	}
 
-	int iResult = g_PropDataSystem.ParsePropFromKV( this, pkvPropData, modelKeyValues );
+	int iResult = g_PropDataSystem.ParsePropFromKV( this, this, pkvPropData, modelKeyValues );
 	modelKeyValues->deleteThis();
 	return iResult;
 }
@@ -902,14 +902,14 @@ void C_FuncPhysicsRespawnZone::InitializePropsWithin( void )
 			pProp->SetRespawnZone( this );
 
 			// This is a crappy way to do this
-			int index = m_PropList.AddToTail();
-			m_PropList[index].iszModelName = pProp->GetModelName();
-			m_PropList[index].vecOrigin = pProp->GetAbsOrigin();
-			m_PropList[index].vecAngles = pProp->GetAbsAngles();
-			m_PropList[index].iSkin = pProp->m_nSkin;
-			m_PropList[index].iHealth = pProp->m_iHealth;
-			m_PropList[index].iSpawnFlags = pProp->m_spawnflags;
-			m_PropList[index].hClientEntity = pProp->GetClientHandle();
+			int iProp = m_PropList.AddToTail();
+			m_PropList[iProp].iszModelName = pProp->GetModelName();
+			m_PropList[iProp].vecOrigin = pProp->GetAbsOrigin();
+			m_PropList[iProp].vecAngles = pProp->GetAbsAngles();
+			m_PropList[iProp].iSkin = pProp->m_nSkin;
+			m_PropList[iProp].iHealth = pProp->m_iHealth;
+			m_PropList[iProp].iSpawnFlags = pProp->m_spawnflags;
+			m_PropList[iProp].hClientEntity = pProp->GetClientHandle();
 		}
 	}
 }
