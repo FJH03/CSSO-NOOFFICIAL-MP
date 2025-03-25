666d8e34602f4b0afbdf066a43144b9e11f29b79
DMK95 <maga200544@gmail.com>
2025-03-25 21:18:36 +0300

    Revert "(test) new vote input"
    This reverts commit d4739d67c32447c1eab235edc7b3b56650c388e4.

--- game/client/hud_vote.cpp
+++ game/client/hud_vote.cpp
@@ -976,6 +976,9 @@ int	CHudVote::KeyInput( int down, ButtonCode_t keynum, const char *pszCurrentBin
 	if ( !IsVisible() )
 		return 1;
 
+	if ( !down )
+		return 1;
+
 	if ( !m_bVotingActive )
 		return 1;
 
@@ -985,28 +988,25 @@ int	CHudVote::KeyInput( int down, ButtonCode_t keynum, const char *pszCurrentBin
 	if ( !m_bShowVoteActivePanel )
 		return 1;
 
-	if (!pszCurrentBinding)
-        return 1;
-
 	int nSlot = 999;
 
-	if ( !Q_strcmp(pszCurrentBinding, "slot1") )
+	if ( down && keynum == KEY_1 )
 	{
 		nSlot = 1;
 	}
-	else if ( !Q_strcmp(pszCurrentBinding, "slot2") )
+	else if ( down && keynum == KEY_2 )
 	{
 		nSlot = 2;
 	}
-	else if ( !Q_strcmp(pszCurrentBinding, "slot3") )
+	else if ( down && keynum == KEY_3 )
 	{
 		nSlot = 3;
 	}
-	else if ( !Q_strcmp(pszCurrentBinding, "slot4") )
+	else if ( down && keynum == KEY_4 )
 	{
 		nSlot = 4;
 	}
-	else if ( !Q_strcmp(pszCurrentBinding, "slot5") )
+	else if ( down && keynum == KEY_5 )
 	{
 		nSlot = 5;
 	}
