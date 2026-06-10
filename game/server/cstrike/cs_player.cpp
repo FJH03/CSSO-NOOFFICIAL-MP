a7e9dda7ebc80febfe18ab3eb6570c058611905a
FJH_03 <fengjiahe5@gmail.com>
2026-06-10 19:54:07 +0800

    Revert "Allow Spam Inspect"
    This reverts commit 23f49dd5bc6ef149852f09be91ec6ee0f4753fa9.

--- game/server/cstrike/cs_player.cpp
+++ game/server/cstrike/cs_player.cpp
@@ -6905,8 +6905,6 @@ bool CCSPlayer::ClientCommand( const CCommand &args )
 	}
 	else if ( FStrEq( pcmd, "+lookatweapon" ) )
 	{
-		StopLookingAtWeapon();
-		
 		m_bIsHoldingLookAtWeapon = true;
 
 		if ( ShouldRunRateLimitedCommand( args ) )
