e4faeba122c35d4b3f50d5f52c023b066ba7a5d9
FJH_03 <fengjiahe5@gmail.com>
2026-07-11 13:42:06 +0800

    补全rendermodel缓存

--- particles/particles.cpp
+++ particles/particles.cpp
@@ -1087,6 +1087,12 @@ void CParticleCollection::Init( CParticleSystemDefinition *pDef, float flDelay, 
 	m_Sheet.Set( g_pParticleSystemMgr->FindOrLoadSheet( pDef ) );
 	m_bTriedLoadingSheet = false;
 
+	// Ensure render operators are precached (Precache during DLL init runs before renderers are parsed)
+	for ( int i = 0; i < pDef->m_Renderers.Count(); i++ )
+	{
+		pDef->m_Renderers[i]->Precache();
+	}
+
 	// FIXME: This seed needs to be recorded per instance!
 	m_bIsScrubbable = ( nRandomSeed != 0 );
 	if ( m_bIsScrubbable )
